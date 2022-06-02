eb737830b79d8850cf2dd1b2aeff64ed92d2fa69
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2022-06-02 10:32:05 -0300

    funciones de arbol

--- TDAArbolBinario/arbol.h
+++ TDAArbolBinario/arbol.h
@@ -6,21 +6,43 @@
 #define SIN_MEM 2
 #define DUPLICADO 3
 
-typedef int (*Cmp)(const void*,const void*);
-
+typedef int (*Cmp)(const void *, const void *);
+typedef int (*Accion)(void *elem, void *datosAccion);
+typedef int(*Actualizar)(void* elem,const void* dato);
+typedef int(*Imprimir)(void* elem,const void* dato,int nivel);
 typedef struct sNodo
 {
-    void* elem;
+    void *elem;
     size_t tamElem;
-    struct sNodo* izq;
-    struct sNodo* der;
+    struct sNodo *izq;
+    struct sNodo *der;
 } NodoA;
 
-typedef NodoA* Arbol;
+typedef NodoA *Arbol;
 
-void crearArbol(Arbol* pa);
-NodoA* crearNodo(void* dato,size_t tamElem);
-int insertarEnArbol(Arbol* pa,void* dato,size_t tamElem,Cmp cmp);
-int buscarNodo(Arbol* pa,void* dato,size_t tamElem,Cmp cmp,void* dst);
+typedef enum
+{
+    COMPLETO,
+    BALANCEADO,
+    AVL,
+    DESBALANCEADO
+} TipoArbol;
+
+void crearArbol(Arbol *pa);
+NodoA *crearNodo(void *dato, size_t tamElem);
+int insertarEnArbolRecursivo(Arbol *pa, const void *dato, size_t tamElem, Cmp cmp,Actualizar actualizar);
+int insertarEnArbolIterativo(Arbol *pa, const void *dato, size_t tamElem, Cmp cmp, Actualizar actualizar);
+int buscarNodo(Arbol *pa, void *dato, size_t tamElem, Cmp cmp, void *dst);
+int arbolVacio(Arbol *pa);
+void recorrerArbolPreOrden(Arbol *pa, Accion accion, void *datosAccion);
+void recorrerArbolEnOrden(Arbol *pa, Accion accion, void *datosAccion);
+void recorrerArbolPosOrden(Arbol *pa, Accion accion, void *datosAccion);
+void vaciarArbol(Arbol *pa);
+int esArbolCompleto(const Arbol *pa);
+int esArbolBalanceado(const Arbol *pa);
+int esArbolAVL(const Arbol *pa);
+TipoArbol tipoArbol(const Arbol *pa);
+void imprimirArbolRecursiva(Arbol *pa, Imprimir accion, void *datosAccion, int nivel);
+void imprimirArbol(Arbol *pa, Imprimir accion, void *datosAccion, int nivel);
 
 #endif // ARBOL_H_INCLUDED
--- TDAArbolBinario/main.c
+++ TDAArbolBinario/main.c
@@ -1,26 +1,24 @@
-#include<stdlib.h>
-#include<mem.h>
+#include <stdlib.h>
+#include <mem.h>
 #include "./arbol.h"
 
-
-void crearArbol(Arbol* pa)
+void crearArbol(Arbol *pa)
 {
-    *pa=NULL;
+    *pa = NULL;
 }
 
-
-NodoA* crearNodo(void* dato,size_t tamElem)
+NodoA *crearNodo(void *dato, size_t tamElem)
 {
-    NodoA* nai = (NodoA*) malloc(sizeof(NodoA));
-    void* elem = malloc(tamElem);
+    NodoA *nai = (NodoA *)malloc(sizeof(NodoA));
+    void *elem = malloc(tamElem);
 
-    if(!nai || !elem)
+    if (!nai || !elem)
     {
         free(nai);
         free(elem);
         return NULL;
     }
-    memcpy(elem,dato,tamElem);
+    memcpy(elem, dato, tamElem);
     nai->der = NULL;
     nai->izq = NULL;
     nai->tamElem = tamElem;
@@ -28,13 +26,12 @@ NodoA* crearNodo(void* dato,size_t tamElem)
     return nai;
 }
 
-
-int insertarEnArbol(Arbol* pa,void* dato,size_t tamElem,Cmp cmp)
+int insertarEnArbolRecursivo(Arbol *pa, const void *dato, size_t tamElem, Cmp cmp, Actualizar actualizar)
 {
-    if(!*pa)
+    if (!*pa)
     {
-        NodoA* nai = crearNodo(dato,tamElem);
-        if(!nai)
+        NodoA *nai = crearNodo(dato, tamElem);
+        if (!nai)
         {
             return SIN_MEM;
         }
@@ -42,43 +39,130 @@ int insertarEnArbol(Arbol* pa,void* dato,size_t tamElem,Cmp cmp)
         return VERDADERO;
     }
 
-    int auxCmp = cmp((*pa)->elem,dato);
+    int auxCmp = cmp((*pa)->elem, dato);
 
-    if(auxCmp == 0)
-        return DUPLICADO;
-
-    if(auxCmp > 0)
+    if (auxCmp == 0)
     {
-        return insertarEnArbol(&(*pa)->izq,dato,tamElem,cmp);
+        if (actualizar)
+        {
+            actualizar((*pa)->elem, dato);
+        }
+        return DUPLICADO;
     }
-    return insertarEnArbol(&(*pa)->der,dato,tamElem,cmp);
 
+    return insertarEnArbolRecursivo(auxCmp < 0 ? &(*pa)->izq : &(*pa)->der, dato, tamElem, cmp, actualizar);
 }
 
-
-int buscarNodo(Arbol* pa,void* dato,size_t tamElem,Cmp cmp,void* dst)
+int insertarEnArbolIterativo(Arbol *pa, const void *dato, size_t tamElem, Cmp cmp, Actualizar actualizar)
 {
-    if(!*pa)
+    int comp;
+
+    while (*pa)
+    {
+        comp = cmp(dato, (*pa)->elem);
+
+        if (comp == 0)
+        {
+            if (actualizar)
+                return actualizar((*pa)->elem, dato);
+            return DUPLICADO;
+        }
+
+        if (comp < 0)
+            pa = &(*pa)->izq;
+        else
+            pa = &(*pa)->der;
+    }
+
+    NodoA *nai = crearNodo(dato, tamElem);
+    if (!nai)
+        return SIN_MEM;
+    *pa = nai;
+    return VERDADERO;
+}
+
+int buscarNodo(Arbol *pa, void *dato, size_t tamElem, Cmp cmp, void *dst)
+{
+    if (!*pa)
         return FALSO;
 
-    int auxCmp = cmp((*pa)->elem,dato);
+    int auxCmp = cmp((*pa)->elem, dato);
 
-    if(auxCmp == 0)
+    if (auxCmp == 0)
     {
-        memcpy(dst,dato,tamElem);
+        memcpy(dst, dato, tamElem);
         return VERDADERO;
     }
-    if(auxCmp > 0)
+    if (auxCmp > 0)
     {
 
-        return buscarNodo(&(*pa)->izq,dato,tamElem,cmp,dst);
+        return buscarNodo(&(*pa)->izq, dato, tamElem, cmp, dst);
     }
     else
     {
-        return buscarNodo(&(*pa)->der,dato,tamElem,cmp,dst);
+        return buscarNodo(&(*pa)->der, dato, tamElem, cmp, dst);
     }
 }
 
+int arbolVacio(Arbol *pa)
+{
+}
 
+void recorrerArbolPreOrden(Arbol *pa, Accion accion, void *datosAccion)
+{
+    if (!*pa)
+        return;
 
+    accion((*pa)->elem, datosAccion);
+    recorrerArbolPreOrden(&(*pa)->izq, accion, datosAccion);
+    recorrerArbolPreOrden(&(*pa)->der, accion, datosAccion);
+}
 
+void recorrerArbolEnOrden(Arbol *pa, Accion accion, void *datosAccion)
+{
+    if (!*pa)
+        return;
+
+    recorrerArbolEnOrden(&(*pa)->izq, accion, datosAccion);
+    accion((*pa)->elem, datosAccion);
+    recorrerArbolEnOrden(&(*pa)->der, accion, datosAccion);
+}
+
+void recorrerArbolPosOrden(Arbol *pa, Accion accion, void *datosAccion)
+{
+    if (!*pa)
+        return;
+
+    recorrerArbolPosOrden(&(*pa)->izq, accion, datosAccion);
+    recorrerArbolPosOrden(&(*pa)->der, accion, datosAccion);
+    accion((*pa)->elem, datosAccion);
+}
+void vaciarArbol(Arbol *pa)
+{
+}
+int esArbolCompleto(const Arbol *pa)
+{
+}
+int esArbolBalanceado(const Arbol *pa)
+{
+}
+int esArbolAVL(const Arbol *pa)
+{
+}
+TipoArbol tipoArbol(const Arbol *pa)
+{
+}
+void imprimirArbolRecursiva(Arbol *pa, Imprimir accion, void *datosAccion, int nivel)
+{
+    if (!*pa)
+        return;
+
+    imprimirArbol(&(*pa)->izq, accion, datosAccion, nivel + 1);
+    accion((*pa)->elem, datosAccion, nivel);
+    imprimirArbol(&(*pa)->der, accion, datosAccion, nivel + 1);
+}
+
+void imprimirArbol(Arbol *pa, Imprimir accion, void *datosAccion, int nivel)
+{
+    imprimirArbolRecursiva(pa, accion, datosAccion, 0);
+}
