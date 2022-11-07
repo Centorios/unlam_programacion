1baa42d7f4fceef81b1b8a16b1ac8dbcb9be7711
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2022-11-07 04:25:57 -0300

    back to portrait

--- Marlin-2.0.9.5/Marlin/Configuration.h
+++ Marlin-2.0.9.5/Marlin/Configuration.h
@@ -2978,8 +2978,8 @@
 //
 //#define DWIN_CREALITY_LCD           // Creality UI
 //#define DWIN_LCD_PROUI              // Pro UI by MRiscoC
-#define DWIN_CREALITY_LCD_JYERSUI   // Jyers UI by Jacob Myers
-//#define DWIN_MARLINUI_PORTRAIT      // MarlinUI (portrait orientation)
+//#define DWIN_CREALITY_LCD_JYERSUI   // Jyers UI by Jacob Myers
+#define DWIN_MARLINUI_PORTRAIT      // MarlinUI (portrait orientation)
 //#define DWIN_MARLINUI_LANDSCAPE       // MarlinUI (landscape orientation)
 
 //
